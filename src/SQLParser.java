a0e5a3895855e2a1e4a7a3dafdc59bf40d70599b
xsuler <aeonsule@gmail.com>
2019-04-08 11:33:01 -0400

    error handling

--- src/SQLParser.java
+++ src/SQLParser.java
@@ -13,19 +13,29 @@ public class SQLParser{
 		ANTLRInputStream input = new ANTLRInputStream( System.in);
 
 		MiniSQLLexer lexer = new MiniSQLLexer(input);
+		lexer.removeErrorListeners();
+		lexer.addErrorListener(ThrowingErrorListener.INSTANCE);
 
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 
 		MiniSQLParser parser = new MiniSQLParser(tokens);
-		ParseTree tree = parser.sql(); // begin parsing at rule 'r'
-		
-		MyListener extractor = new MyListener();
-		ParseTreeWalker walker=new ParseTreeWalker();
-		walker.walk(extractor, tree);
-		
-		
-		System.out.println(tree.toStringTree(parser)); // print LISP-style tree
-		
+		parser.removeErrorListeners();
+		parser.addErrorListener(ThrowingErrorListener.INSTANCE);
+
+		try {
+			ParseTree tree = parser.sql(); // begin parsing at rule 'r'
+			
+			MyListener extractor = new MyListener();
+			ParseTreeWalker walker=new ParseTreeWalker();
+			walker.walk(extractor, tree);
+			
+			
+			System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+		}
+		catch(Exception e) {
+			System.out.println("error");
+		}
+
 		/*switch(eval.type) {
 		case Statement.create:
 			StatementCreate sc=eval.sc;
