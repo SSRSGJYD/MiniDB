3d682bcc7fab4b8f19eae24c7d060bfeeea26e75
xsuler <aeonsule@gmail.com>
2019-05-08 11:35:41 +0800

    fix bugs

--- src/SQLParser.java
+++ src/SQLParser.java
@@ -20,17 +20,17 @@ public class SQLParser{
 	{
 		String cmds="create database db\n"
 				+ "use database db\n"
-				+ "create table playr(id char(4) not null,name int not null,primary key(id))\n"
+				+ "create table playr(id char(4) not null,name char(4) not null,primary key(id))\n"
 				+ "create table playt(id int,name int,primary key(id))\n"
 
-				+ "insert into playr values('asdd',90)\n"
-				+ "insert into playr values('1333',990)\n"
+				+ "insert into playr values('asdd','asds')\n"
+				+ "insert into playr values('1333','990d')\n"
 
 				+ "insert into playt values(133,90)\n"
 				+ "insert into playt(id) values(134)\n"
 				+ "insert into playt values(13,990)\n"
 
-				+ "select * from playr where id>'1333'\n"
+				+ "select * from playr where name>'asds'\n"
 //				+ "select * from playr join playt on playr.name=playt.name\n"
 
 				+ "drop table playr\n"
